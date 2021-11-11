955f3c3dea9930584adc1eadcc0020a6e75466cc
Newave <9218518+RealNewave@users.noreply.github.com>
2021-11-11 17:11:45 +0100

    Add files via upload

--- src/main/java/com/example/azureapp/AzureAppApplication.java
+++ src/main/java/com/example/azureapp/AzureAppApplication.java
@@ -2,8 +2,10 @@ package com.example.azureapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class AzureAppApplication {
 
     public static void main(String[] args) {
